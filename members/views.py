a4e14db21a5aa611389467b3343718fa0ea384e9
Tafsir Haque Arnob <tharnob@gmail.com>
2024-01-01 03:24:19 +0600

    Full Authentication System has developed!

---
+++ members/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
+# Create your views here.
+
+
+def login_user(request):
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('/dashboard')
+        else:
+            # Return an 'invalid login' error message.
+            messages.success(request, ("There was an error logging in, Try Again..."))
+            return redirect('/members/login_user/')
+            
+
+
+    else:
+        return render(request, 'authenticate/login.html', {})
+    
+
+
+def logout_user(request):
+    logout(request)
+    # messages.success(request, ("You are Logged Out!"))
+    return redirect('/')
+
+
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            # messages.success(request, ("Registration Successful!"))
+            return redirect('/')
+        
+    else:
+        form = RegisterUserForm()
+
+
+    return render(request, 'authenticate/register_user.html', { 'form':form })
