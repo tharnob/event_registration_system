18689a633f731c2e9a8967bc66c25618009ee4ac
Tafsir Haque Arnob <tharnob@gmail.com>
2024-01-01 13:14:22 +0600

    Event has been added to default admin page for crud operation

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Event
 
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "event_date", "event_time", "location_name", "slot", "created_at",)
+
+admin.site.register(Event, EventAdmin)
